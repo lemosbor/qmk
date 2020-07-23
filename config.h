cdb6f776be070ddf27f475175d6a60a7284c51a4
lemosbor <68635720+lemosbor@users.noreply.github.com>
2020-07-23 16:43:44 +0300

    Update config.h

--- config.h
+++ config.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #ifdef AUDIO_ENABLE
-    #define STARTUP_SONG SONG(PREONIC_SOUND)
+    #define STARTUP_SONG SONG(ODE_TO_JOY)
     // #define STARTUP_SONG SONG(NO_SOUND)
+    //  #define STARTUP_SONG SONG(PREONIC_SOUND)
 
     #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
                                   SONG(COLEMAK_SOUND), \
