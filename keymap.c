ad7ad0893bbbfbfe0583721983b8480ab2753b7b
lemosbor <68635720+lemosbor@users.noreply.github.com>
2020-07-23 18:03:18 +0300

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -3,7 +3,7 @@
 
 enum preonic_layers {
   _QWERTY,
-  _COLEMAK,
+  _SERV,
   _DVORAK,
   _LOWER,
   _RAISE,
@@ -12,16 +12,34 @@ enum preonic_layers {
 
 enum preonic_keycodes {
   QWERTY = SAFE_RANGE,
-  COLEMAK,
+  SERV,
   DVORAK,
   LOWER,
   RAISE,
   BACKLIT
 };
 
+enum unicode_names {
+    BANG,
+    IRONY,
+    SNEK,
+    JE,
+    JEL
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+    [BANG]  = 0x203D,  // ‽
+    [IRONY] = 0x2E2E,  // ⸮
+    [SNEK]  = 0x1F40D, // 🐍
+    [JE]  = 0x0436, // ж
+    [JEL]  = 0x0416, // Ж
+  
+};
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Qwerty
+/* Qwerty (KC_GRV — (тире —))
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  ВШ  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -38,8 +56,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  \
   KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP, KC_ENT,  \
-  KC_LCTL, KC_LGUI, KC_RALT, KC_GRV, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_DEL, KC_LEFT, KC_DOWN,   KC_RGHT  \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, XP(JE,JEL),  KC_UP, KC_ENT,  \
+  KC_LCTL, KC_LGUI, KC_RALT, X(SNEK), SERV,   KC_SPC,  KC_SPC,  RAISE,   KC_DEL, KC_LEFT, KC_DOWN,   KC_RGHT  \
 ),
 
 /* сервисная
