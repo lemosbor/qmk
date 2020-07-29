b9cb13bae2e868b4bf3cca79fbb4b130e4e573e7
lemosbor <68635720+lemosbor@users.noreply.github.com>
2020-07-29 16:32:36 +0300

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -1,5 +1,12 @@
-SRC += muse.c
-UNICODEMAP_ENABLE = yes
-RGBLIGHT_ENABLE = yes
-BACKLIGHT_ENABLE = yes
-TAP_DANCE_ENABLE = yes
+#включить:
+UNICODEMAP_ENABLE = yes   # поддержка карт юникода
+RGBLIGHT_ENABLE = yes     # поддержка светодиодов клавиш
+BACKLIGHT_ENABLE = yes    # поддержка светодиодов платы
+TAP_DANCE_ENABLE = yes    # поддержка двойных нажатий
+VELOCIKEY_ENABLE = yes    # подстройка подсветки под скорость печати
+EXTRAKEY_ENABLE = yes     # аудиоконтроль и системный контроль
+
+#отключить:
+BLUETOOTH_ENABLE = no     # поддержка блютуза
+UNICODE_ENABLE = no       # поддержка юникода
+BOOTMAGIC_ENABLE = no     # виртуальная настройка клавиатуры
