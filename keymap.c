b7dae789f28f9b192af27f6636ba3d13a62e2ca3
lemosbor <68635720+lemosbor@users.noreply.github.com>
2020-08-03 20:44:11 +0300

    Add files via upload

--- keymap.c
+++ keymap.c
@@ -43,15 +43,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { // определ
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | РЕГ  |   Я  |   Ч  |   С  |   М  |   И  |   Т  |   Ь  |   Б  |   Ю  |   ↑  | ВВОД |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | УПР  | Окно | ДОП  |   /  |  Б/Ц |    пробел   |  Р/А |  УД  |   ←  |   ↓  |  →   |
+ * | УПР  | Окно | ДОП  |   \  |  Б/Ц |    пробел   |  Р/А |  УД  |   ←  |   ↓  |  →   |
  * `-----------------------------------------------------------------------------------'
  */
 [L_OSNOVA] = LAYOUT_preonic_grid( \
-  TD(VYH),  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  TD(TABB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  \
-  KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  TD(VYH),  KC_1,    KC_2,  KC_5,   KC_4,   KC_3,    KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+  KC_MINS,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  \
+  TD(TABB),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP, KC_ENT,  \
-  KC_LCTL, TD(WEMO), KC_RALT, KC_GRV, TD(PER_LAY),   KC_SPC,  KC_SPC,  KC_MINS,   KC_DEL, KC_LEFT, KC_DOWN,   KC_RGHT  \
+  KC_LCTL, TD(WEMO), KC_RALT, KC_BSLS, TD(PER_LAY),   KC_SPC,  KC_SPC, KC_CAPS,   KC_DEL, KC_LEFT, KC_DOWN,   KC_RGHT  \
 ),
 /* сервисная
  * ,-----------------------------------------------------------------------------------.
@@ -67,11 +67,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { // определ
  * `-----------------------------------------------------------------------------------'
  */
 [L_SERV] = LAYOUT_preonic_grid( \
-  KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_NLCK, KC_PSLS, KC_PAST, KC_BSPC, \
-  KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_SLSH, KC_HOME, KC_PGUP, KC_P7, KC_P8, KC_P9, KC_PPLS,\
+  KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_PAST, KC_PSLS, KC_PMNS, KC_BSPC, \
+  KC_F9, KC_F10, KC_F11, KC_F12, KC_NLCK, KC_SLSH, KC_HOME, KC_PGUP, KC_P7, KC_P8, KC_P9, KC_PPLS,\
   RGB_MOD, RGB_TOG, KC_MS_U, KC_NO, KC_NO, KC_INS, KC_END, KC_PGDN, KC_P4, KC_P5, KC_P6, KC_EQL,\
-  KC_LSFT, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO, KC_BSLS, KC_LBRC, KC_RBRC, KC_P1, KC_P2, KC_P3, KC_ENT,\
-  KC_LCTL, KC_BTN1, KC_LALT, KC_BTN2, TD(PER_LAY), KC_SPC, KC_SPC, KC_MINS, KC_5, KC_P0, KC_PDOT, KC_RGHT \
+  KC_LSFT, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO, KC_NO, KC_NO, KC_RBRC, KC_P1, KC_P2, KC_P3, KC_ENT,\
+  KC_LCTL, KC_BTN1, KC_LALT, KC_BTN2, TD(PER_LAY), KC_SPC, KC_SPC, KC_NO, KC_5, KC_P0, KC_PDOT, KC_TAB \
 )
 };
 
