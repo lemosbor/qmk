eede21558ee9a64b8fbd1c8ac46d0c4d5b4c3820
lemosbor <68635720+lemosbor@users.noreply.github.com>
2020-08-25 17:43:43 +0300

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -25,7 +25,8 @@ enum {
     VYH, // Вых / альт+Ф4
     TABB, // таб / альт+таб
     WEMO, // окно / окно+точка
-    RU_AN // кнопка Р/А
+    RU_AN, // кнопка Р/А
+    KOPY
 };
 uint8_t cur_dance(qk_tap_dance_state_t *state); // общая функция нажатий
 void ql_finished(qk_tap_dance_state_t *state, void *user_data); //индивидуальные функции двойных нажатий
@@ -42,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { // определ
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | РЕГ  |   Я  |   Ч  |   С  |   М  |   И  |   Т  |   Ь  |   Б  |   Ю  |   ↑  | ВВОД |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | УПР  | Окно | ДОП  |   \  |  Р/А |    пробел   |  Б/Ц |  УД  |   ←  |   ↓  |  →   |
+ * | УПР  | Окно | ДОП  | к/в  |  Р/А |    пробел   |  Б/Ц |  УД  |   ←  |   ↓  |  →   |
  * `-----------------------------------------------------------------------------------'
  */
 [L_OSNOVA] = LAYOUT_preonic_grid( \
@@ -50,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { // определ
   TD(TABB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  \
   TD(VYH),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP, KC_ENT,  \
-  KC_LCTL, TD(WEMO), KC_LALT, KC_BSLS, TD(RU_AN), KC_SPC,  KC_SPC, TD(PER_LAY), KC_DEL, KC_LEFT, KC_DOWN,   KC_RGHT  \
+  KC_LCTL, TD(WEMO), KC_LALT, TD(KOPY), TD(RU_AN), KC_SPC,  KC_SPC, TD(PER_LAY), KC_DEL, KC_LEFT, KC_DOWN,   KC_RGHT  \
 ),
 /* сервисная
  * ,-----------------------------------------------------------------------------------.
@@ -60,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { // определ
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |цвет р|цвет в|М.верх| вст  |      | печ  |      | вниз |   4  |   5  |   6  |  =   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | РЕГ  |М.лево|М.вниз|М.прав|      |      |      |   Ъ  |   1  |   2  |   3  | ВВОД |
+ * | РЕГ  |М.лево|М.вниз|М.прав|      |      |   \  |   Ъ  |   1  |   2  |   3  | ВВОД |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | УПР  | М.ЛК | ДОП  | М.ПК |  ЦИФ |    пробел   |  Б/Ц |   ,  |   0  |   .  | ТАБ  |
  * `-----------------------------------------------------------------------------------'
@@ -69,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { // определ
   KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_PAST, KC_PSLS, KC_PMNS, KC_BSPC, \
   KC_F9, KC_F10, KC_F11, KC_F12, KC_END, KC_SLSH, KC_HOME, KC_PGUP, KC_P7, KC_P8, KC_P9, KC_PPLS,\
   RGB_MOD, RGB_TOG, KC_MS_U, KC_INS, KC_NO, KC_PSCREEN, KC_NO, KC_PGDN, KC_P4, KC_P5, KC_P6, KC_EQL,\
-  KC_LSFT, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO, KC_NO, KC_NO, KC_RBRC, KC_P1, KC_P2, KC_P3, KC_ENT,\
+  KC_LSFT, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO, KC_NO, KC_BSLS, KC_RBRC, KC_P1, KC_P2, KC_P3, KC_ENT,\
   KC_LCTL, KC_BTN1, KC_LALT, KC_BTN2, KC_NLCK, KC_SPC, KC_SPC, TD(PER_LAY), KC_5, KC_P0, KC_PDOT, KC_TAB \
 )
 };
@@ -123,7 +124,25 @@ void x_reset(qk_tap_dance_state_t *state, void *user_data) { // Действие
         case SINGLE_HOLD: unregister_code(KC_RALT); break;
     }
     ql_tap_state.state = 0;
-}
+};
+
+void kop_finished(qk_tap_dance_state_t *state, void *user_data) { // функция реакции двойного нажатия к/в. Действие при нажатии
+    ql_tap_state.state = cur_dance(state);
+    switch (ql_tap_state.state) {
+        case SINGLE_TAP: register_code16(LCTL(KC_C)); break;
+        case SINGLE_HOLD: register_code16(LCTL(KC_X)); break;
+        case DOUBLE_TAP: register_code16(LCTL(KC_V)); break; // двойное нажатие удержание. Постоянное переключение слоя
+    }
+};
+void kop_reset(qk_tap_dance_state_t *state, void *user_data) { // Действие при отпускании (то отключить слой)
+    switch (ql_tap_state.state) {
+        case SINGLE_TAP: unregister_code16(LCTL(KC_C)); break;
+        case SINGLE_HOLD: unregister_code16(LCTL(KC_X)); break;
+        case DOUBLE_TAP: unregister_code16(LCTL(KC_V)); break;
+    }
+    ql_tap_state.state = 0; // обнуление состояния
+};
+
 
 qk_tap_dance_action_t tap_dance_actions[] = { // связка кнопок с функциями двойного нажатия
     [PER_LAY] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, ql_finished, ql_reset, 275), // Б/Ц
@@ -132,35 +151,16 @@ qk_tap_dance_action_t tap_dance_actions[] = { // связка кнопок с ф
     [VYH] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, LALT(KC_F4)), // выход или альт+ф4
     [TABB] = ACTION_TAP_DANCE_DOUBLE(KC_TAB, LALT(KC_TAB)), // таб или альт+таб https://docs.qmk.fm/#/feature_macros?id=super-alt%e2%86%aftab
     [WEMO] = ACTION_TAP_DANCE_DOUBLE(KC_LGUI, LGUI(KC_DOT)), // вин или эмодзи
-    [RU_AN] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, x_finished, x_reset) // Р/А
+    [RU_AN] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, x_finished, x_reset), // Р/А
+    [KOPY] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, kop_finished, kop_reset) // Р/А
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) { //цветовая индикация переключения раскладок
     switch (get_highest_layer(state)) {
     case L_SERV:
-        rgblight_setrgb (0xFF,  0x00, 0x00);
+        rgblight_setrgb (0xFF,  0x00, 0x00);  break;
     case L_OSNOVA:
-        rgblight_setrgb (0x00,  0x00, 0xFF);
+        rgblight_setrgb (0x00,  0x00, 0xFF);  break;
     }
   return state;
-};
-
-//void rgb_matrix_indicators_user(void) { //индикация капса
-    //if (host_keyboard_led_state().caps_lock) {
-      //  rgb_matrix_set_color(0, 0x00, 0xFF, 0x00);
-    //}
-//}
-//индикация сервисной раскладки
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) { // макрос копирования и вставки одной кнопкой
-    switch(id) {
-        case 0: {
-            if (record->event.pressed) {
-                return MACRO( D(LCTL), T(C), U(LCTL), END  );
-            } else {
-                return MACRO( D(LCTL), T(V), U(LCTL), END  );
-            }
-            break;
-        }
-    }
-    return MACRO_NONE;
 };
