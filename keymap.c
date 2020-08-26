f00ba9cb86a20d39548a0d231c99bbb5c31c7e17
lemosbor <68635720+lemosbor@users.noreply.github.com>
2020-08-26 13:25:51 +0300

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -59,19 +59,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { // определ
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |  Ф9  |  Ф10 |  Ф11 |  Ф12 | кон  |  Ё   |  нач | вверх|   7  |   8  |   9  |  +   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |цвет р|цвет в|М.верх| вст  |      | печ  |      | вниз |   4  |   5  |   6  |  =   |
+ * |цвет р|цвет в|      | вст  |      | печ  |      | вниз |   4  |   5  |   6  |  =   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | РЕГ  |М.лево|М.вниз|М.прав|      |      |   \  |   Ъ  |   1  |   2  |   3  | ВВОД |
+ * | РЕГ  | М.ЛК |М.верх| М.ПК |      |      |   \  |   Ъ  |   1  |   2  |   3  | ВВОД |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | УПР  | М.ЛК | ДОП  | М.ПК |  ЦИФ |    пробел   |  Б/Ц |   ,  |   0  |   .  | ТАБ  |
+ * | УПР  |М.лево|М.вниз|М.прав|  ЦИФ |    пробел   |  Б/Ц |   ,  |   0  |   .  | ТАБ  |
  * `-----------------------------------------------------------------------------------'
  */
 [L_SERV] = LAYOUT_preonic_grid( \
   KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_PAST, KC_PSLS, KC_PMNS, KC_BSPC, \
   KC_F9, KC_F10, KC_F11, KC_F12, KC_END, KC_SLSH, KC_HOME, KC_PGUP, KC_P7, KC_P8, KC_P9, KC_PPLS,\
-  RGB_MOD, RGB_TOG, KC_MS_U, KC_INS, KC_NO, KC_PSCREEN, KC_NO, KC_PGDN, KC_P4, KC_P5, KC_P6, KC_EQL,\
-  KC_LSFT, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO, KC_NO, KC_BSLS, KC_RBRC, KC_P1, KC_P2, KC_P3, KC_ENT,\
-  KC_LCTL, KC_BTN1, KC_LALT, KC_BTN2, KC_NLCK, KC_SPC, KC_SPC, TD(PER_LAY), KC_5, KC_P0, KC_PDOT, KC_TAB \
+  RGB_MOD, RGB_TOG, KC_NO , KC_INS, KC_NO, KC_PSCREEN, KC_NO, KC_PGDN, KC_P4, KC_P5, KC_P6, KC_EQL,\
+  KC_LSFT, KC_BTN1, KC_MS_U, KC_BTN2 , KC_NO, KC_NO, KC_BSLS, KC_RBRC, KC_P1, KC_P2, KC_P3, KC_ENT,\
+  KC_LCTL, KC_MS_L, KC_MS_D, KC_MS_R, KC_NLCK, KC_SPC, KC_SPC, TD(PER_LAY), KC_5, KC_P0, KC_PDOT, KC_TAB \
 )
 };
 
