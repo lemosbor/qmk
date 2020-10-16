53398ee46ca3eb72adef0ce9939631f5f9e7cda8
lemosbor <68635720+lemosbor@users.noreply.github.com>
2020-10-16 15:21:06 +0300

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { // определ
  * ,-----------------------------------------------------------------------------------.
  * | ярче |темнее|      |      | вверх|  Ф10 |  Ф11 |  Ф12 |   *  |   /  |   -  |  ВШ  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |сл.пес|      |      |      | вниз |  Ф7  |  Ф8  |  Ф9  |   7  |   8  |   9  |  +   |
+ * |сл.пес|   ~  |   {  |      | вниз |  Ф7  |  Ф8  |  Ф9  |   7  |   8  |   9  |  +   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |цвет р|цвет в|  Ё " |  нач |  кон |  Ф4  |  Ф5  |  Ф6  |   4  |   5  |   6  |  =   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -66,15 +66,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { // определ
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |М.лево|М.вниз|М.прав|  вст |  ЦИФ |    пробел   |  Б/Ц |   \  |   0  |   .  | ТАБ  |
  * `-----------------------------------------------------------------------------------'
- */
+ */ 
 [L_SERV] = LAYOUT_preonic_grid( \
   KC_BRIU, KC_BRID, KC_NO, KC_NO, KC_PGUP, KC_F10, KC_F11, KC_F12, KC_PAST, KC_PSLS, KC_PMNS, KC_BSPC, \
-  KC_MNXT, KC_NO, KC_NO, KC_NO, KC_PGDN, KC_F7, KC_F8, KC_F9, KC_P7, KC_P8, KC_P9, KC_PPLS, \
+  KC_MNXT, KC_TILDE, KC_LEFT_CURLY_BRACE, KC_NO, KC_PGDN, KC_F7, KC_F8, KC_F9, KC_P7, KC_P8, KC_P9, KC_PPLS, \
   RGB_MOD, RGB_TOG, KC_QUOT, KC_HOME, KC_END, KC_F4, KC_F5, KC_F6, KC_P4, KC_P5, KC_P6, KC_EQL, \
   KC_BTN1, KC_MS_U, KC_BTN2, KC_PSCREEN, KC_SLSH, KC_F1, KC_F2, KC_F3, KC_P1, KC_P2, KC_P3, KC_ENT, \
   KC_MS_L, KC_MS_D, KC_MS_R, KC_INS, KC_NLCK, KC_SPC, KC_SPC, TD(PER_LAY), KC_BSLS, KC_P0, KC_PDOT, KC_TAB \
 )
-};
+}; //beta.docs.qmk.fm/using-qmk/advanced-keycodes/keycodes_us_ansi_shifted
 
 uint8_t cur_dance(qk_tap_dance_state_t *state) { // определение состояния двойного нажатия
     if (state->count == 1) { //если нажата один раз
