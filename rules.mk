660490f20c29d1cc0e74e53cb6109441c1a56828
lemosbor <68635720+lemosbor@users.noreply.github.com>
2020-10-28 16:33:13 +0300

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -5,6 +5,7 @@ BACKLIGHT_ENABLE = yes    # поддержка светодиодов платы
 TAP_DANCE_ENABLE = yes    # поддержка двойных нажатий
 VELOCIKEY_ENABLE = yes    # подстройка подсветки под скорость печати
 EXTRAKEY_ENABLE = yes     # аудиоконтроль и системный контроль
+AUTO_SHIFT_ENABLE = yes   # автоматический Шифт (при лёгком удержании)
 
 #отключить:
 BLUETOOTH_ENABLE = no     # поддержка блютуза
