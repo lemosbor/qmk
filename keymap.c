94b67cb272cb11a1b48ae9d96ede38a49a1da6a9
lemosbor <68635720+lemosbor@users.noreply.github.com>
2020-11-01 10:08:18 +0300

    Add files via upload

--- keymap.c
+++ keymap.c
@@ -19,13 +19,11 @@ enum {
     SINGLE_HOLD,
     DOUBLE_TAP,    
     PER_LAY, // кнопка преключения раскладки
-    VVERH, // кнопка вверх
-    VNIZ, // кнопка вниз
     VYH, // Вых / альт+Ф4
     TABB, // таб / альт+таб
     WEMO, // окно / окно+точка
     RU_AN, // кнопка Р/А
-    KOPY
+    PROB_REG // пробел / регистр
 };
 
 enum combos {
@@ -47,23 +45,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { // определ
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |   "  |   .  |   Y  |   V  |   [  |  ВШ  |   ]  |   Z  |   P  |   B  |   H  |   ;  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | УПР  | Окно | ДОП  |  Б/Ц |пробел|  РЕГ | ВВОД |пробел|  Р/А |  к/в |  НАЧ | КОН  |
+ * | УПР  | Окно | ДОП  |  Б/Ц |пробел|  РЕГ | ВВОД |пробел|  Р/А | БЛОК |  НАЧ | КОН  |
  * `-----------------------------------------------------------------------------------'
  */
 [L_OSNOVA] = LAYOUT_preonic_grid( \
-  KC_1,    KC_2,     KC_3,   KC_4,        KC_5,    KC_DEL,   KC_EQL,  KC_6,    KC_7,   KC_8,     KC_9,     KC_0, \
-  KC_X,    KC_Q,     KC_U,   KC_COMM,     KC_F,    TD(TABB), KC_SLSH, KC_J,   KC_G,     KC_W,     KC_M,    KC_C,  \
-  KC_I,    KC_E,     KC_O,   KC_A,        KC_S,    TD(VYH),  KC_D,    KC_K,   KC_N,     KC_L,     KC_T,    KC_R, \
-  KC_QUOT, KC_DOT,   KC_Y,   KC_V,        KC_LBRC, KC_BSPC,  KC_RBRC, KC_Z,   KC_P,     KC_B,     KC_H,    KC_SCLN, \
-  KC_LCTL, TD(WEMO), KC_RALT,TD(PER_LAY), KC_SPC,  KC_LSFT,  KC_ENT,  KC_SPC, TD(RU_AN),TD(KOPY), KC_HOME, KC_END  \
+  KC_1,    KC_2,     KC_3,   KC_4,        KC_5,    KC_DEL,   KC_EQL,  KC_6,        KC_7,     KC_8,     KC_9,     KC_0, \
+  KC_X,    KC_Q,     KC_U,   KC_COMM,     KC_F,    TD(TABB), KC_SLSH, KC_J,        KC_G,     KC_W,     KC_M,    KC_C,  \
+  KC_I,    KC_E,     KC_O,   KC_A,        KC_S,    TD(VYH),  KC_D,    KC_K,        KC_N,     KC_L,     KC_T,    KC_R, \
+  KC_QUOT, KC_DOT,   KC_Y,   KC_V,        KC_LBRC, KC_BSPC,  KC_RBRC, KC_Z,        KC_P,     KC_B,     KC_H,    KC_SCLN, \
+  KC_LCTL, TD(WEMO), KC_RALT,TD(PER_LAY), KC_SPC,  KC_LSFT,  KC_ENT,  TD(PROB_REG),TD(RU_AN),KC_LOCK, KC_HOME, KC_END  \
 ),
 /* сервисная.
  * ,-----------------------------------------------------------------------------------.
  * | ярче |темнее| тихо |сл.пес| вверх|  УД  |  Ф10 |  Ф11 |  Ф12 |   *  |   /  |   -  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |   ↑  |      | вниз |  ТАБ |  Ф7  |  Ф8  |  Ф9  |   7  |   8  |   9  |
+ * |  коп |   ↑  |  вст | вырез| вниз |  ТАБ |  Ф7  |  Ф8  |  Ф9  |   7  |   8  |   9  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  ←   |   ↓  |   →  |цвет в|  ВЫХ |  Ф4  |  Ф5  |  Ф6  |   4  |   5  |   6  |
+ * |   ←  |   ↓  |   →  | отмен|цвет в|  ВЫХ |  Ф4  |  Ф5  |  Ф6  |   4  |   5  |   6  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | М.ЛК |  м↑  | М.ПК |  ПЕЧ |цвет р|  ВШ  |  Ф1  |  Ф2  |  Ф3  |   1  |   2  |   3  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -71,11 +69,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { // определ
  * `-----------------------------------------------------------------------------------'
  */ 
 [L_SERV] = LAYOUT_preonic_grid( \
-  KC_BRIU,  KC_BRID,  KC_MUTE, KC_MNXT,    KC_PGUP, KC_DEL,  KC_F10, KC_F11, KC_F12, KC_PAST, KC_PSLS, KC_PMNS, \
-  C(KC_INS),S(KC_INS),KC_UP,   KC_NO,      KC_PGDN, TD(TABB),KC_F7,  KC_F8,  KC_F9,  KC_P7,   KC_P8,   KC_P9, \
-  KC_NO,    KC_LEFT,  KC_DOWN, KC_RGHT,    RGB_TOG, TD(VYH), KC_F4,  KC_F5,  KC_F6,  KC_P4,   KC_P5,   KC_P6, \
-  KC_BTN1,  KC_MS_U,  KC_BTN2, KC_PSCR,    RGB_MOD, KC_BSPC, KC_F1,  KC_F2,  KC_F3,  KC_P1,   KC_P2,   KC_P3, \
-  KC_MS_L,  KC_MS_D,  KC_MS_R, TD(PER_LAY),KC_SPC,  KC_LSFT, KC_ENT, KC_INS, KC_NLCK,KC_BSLS, KC_P0,   KC_PDOT \
+  KC_BRIU,  KC_BRID,  KC_MUTE,  KC_MNXT,    KC_PGUP, KC_DEL,  KC_F10, KC_F11,      KC_F12, KC_PAST, KC_PSLS, KC_PMNS, \
+  C(KC_INS),KC_UP,    S(KC_INS),C(KC_X),    KC_PGDN, TD(TABB),KC_F7,  KC_F8,       KC_F9,  KC_P7,   KC_P8,   KC_P9, \
+  KC_LEFT,  KC_DOWN,  KC_RGHT,  C(KC_Z),    RGB_TOG, TD(VYH), KC_F4,  KC_F5,       KC_F6,  KC_P4,   KC_P5,   KC_P6, \
+  KC_BTN1,  KC_MS_U,  KC_BTN2,  KC_PSCR,    RGB_MOD, KC_BSPC, KC_F1,  KC_F2,       KC_F3,  KC_P1,   KC_P2,   KC_P3, \
+  KC_MS_L,  KC_MS_D,  KC_MS_R,  TD(PER_LAY),KC_SPC,  KC_LSFT, KC_ENT, TD(PROB_REG),KC_NLCK,KC_BSLS, KC_P0,   KC_PDOT \
 )
 }; //beta.docs.qmk.fm/using-qmk/advanced-keycodes/keycodes_us_ansi_shifted
 
@@ -134,32 +132,31 @@ void x_reset(qk_tap_dance_state_t *state, void *user_data) { // Действие
     ql_tap_state.state = 0; // обнуление состояния
 };
 
-void kop_finished(qk_tap_dance_state_t *state, void *user_data) { // функция реакции двойного нажатия к/в. Действие при нажатии
+void prreg_finished(qk_tap_dance_state_t *state, void *user_data) { // функция реакции двойного нажатия Р/А. Действие при нажатии
     ql_tap_state.state = cur_dance(state);
     switch (ql_tap_state.state) {
-        case SINGLE_TAP: register_code16(LCTL(KC_V)); break; // нажатие.  Вставить
-        case SINGLE_HOLD: register_code16(LCTL(KC_X)); break; // удержание. Вырезать
-        case DOUBLE_TAP: register_code16(LCTL(KC_C)); break; // двойное нажатие.  Копировать
+        case SINGLE_TAP: register_code(KC_SPC); break;
+        case SINGLE_HOLD: register_code(KC_LSFT); break;
+        case DOUBLE_TAP: register_code(KC_BSPC); break;
     }
-};
-void kop_reset(qk_tap_dance_state_t *state, void *user_data) { // Действие при отпускании (то отключить слой)
+}
+void prreg_reset(qk_tap_dance_state_t *state, void *user_data) { // Действие при отпускании (то отключить слой)
     switch (ql_tap_state.state) {
-        case SINGLE_TAP: unregister_code16(LCTL(KC_V)); break;
-        case SINGLE_HOLD: unregister_code16(LCTL(KC_X)); break;
-        case DOUBLE_TAP: unregister_code16(LCTL(KC_C)); break;
+        case SINGLE_TAP: unregister_code(KC_SPC); break;
+        case SINGLE_HOLD: unregister_code(KC_LSFT); break;
+        case DOUBLE_TAP: unregister_code(KC_BSPC); break;
     }
     ql_tap_state.state = 0; // обнуление состояния
 };
 
+
 qk_tap_dance_action_t tap_dance_actions[] = { // связка кнопок с функциями двойного нажатия
     [PER_LAY] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, ql_finished, ql_reset, 275), // Б/Ц
-    [VVERH] = ACTION_TAP_DANCE_DOUBLE(KC_UP, KC_HOME), // вверх или домой
-    [VNIZ] = ACTION_TAP_DANCE_DOUBLE(KC_DOWN, KC_END), // вниз или в конец
     [VYH] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, LALT(KC_F4)), // выход или альт+ф4
     [TABB] = ACTION_TAP_DANCE_DOUBLE(KC_TAB, LALT(KC_TAB)), // таб или альт+таб https://docs.qmk.fm/#/feature_macros?id=super-alt%e2%86%aftab
     [WEMO] = ACTION_TAP_DANCE_DOUBLE(KC_LGUI, LGUI(KC_DOT)), // вин или эмодзи
     [RU_AN] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, x_finished, x_reset), // Р/А
-    [KOPY] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, kop_finished, kop_reset) // к/в
+    [PROB_REG] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, prreg_finished, prreg_reset) // к/в
 };
 
 void matrix_init_user (void) { //постоянная активация NUMLOCK https://www.reddit.com/r/olkb/comments/5mxtfp/qmk_num_lock/
