1c6890437fe531830b50d22398da53856491bca6
lemosbor <68635720+lemosbor@users.noreply.github.com>
2021-02-08 09:57:19 +0300

    Update config.h

--- XD-75/config.h
+++ XD-75/config.h
@@ -1,7 +1,11 @@
 #pragma once
 #define LOCKING_SUPPORT_ENABLE // блокировка клавиш для капс локов
 #define LOCKING_RESYNC_ENABLE // синхронизация светодиода с каспами
+
 #define TAPPING_TERM 200 // задержка для клавиш двойного назначения
+#define PERMISSIVE_HOLD  // ускорение двойного нажатия
+// #define PERMISSIVE_HOLD_PER_KEY //  усорение двойного нажатия под каждую кнопку
+
 #define COMBO_TERM 20 // задержка для комбо
 #define COMBO_COUNT 47 // количество спец. комбинаций с клавишами
 #define COMBO_ALLOW_ACTION_KEY
