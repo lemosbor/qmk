f8098828591f8f499c74dd587dd0e5701192c6c2
lemosbor <68635720+lemosbor@users.noreply.github.com>
2021-02-08 09:30:26 +0300

    Update rules.mk

--- XD-75/rules.mk
+++ XD-75/rules.mk
@@ -1,17 +1,18 @@
-BACKLIGHT_ENABLE = yes    # поддержка светодиодов клавиш
-TAP_DANCE_ENABLE = yes    # поддержка двойных нажатий
-COMBO_ENABLE = yes        # спец. комбинации из букв
-KEY_LOCK_ENABLE = yes   # блокировка клавиш (удержание)
+# Включить:
+BACKLIGHT_ENABLE = yes   # поддержка светодиодов клавиш
+TAP_DANCE_ENABLE = yes   # поддержка двойных нажатий
+COMBO_ENABLE = yes       # спец. комбинации из букв
+KEY_LOCK_ENABLE = yes    # блокировка клавиш (удержание)
 
-#отключить:
+# Отключить:
 RGBLIGHT_ENABLE = no     # поддержка светодиодов платы
 VELOCIKEY_ENABLE = no    # подстройка подсветки под скорость печати
-BLUETOOTH_ENABLE = no     # поддержка блютуза
-UNICODE_ENABLE = no       # поддержка юникода
+BLUETOOTH_ENABLE = no    # поддержка блютуза
+UNICODE_ENABLE = no      # поддержка юникода
 UNICODEMAP_ENABLE = no   # поддержка карт юникода
-BOOTMAGIC_ENABLE = no     # виртуальная настройка клавиатуры
+BOOTMAGIC_ENABLE = no    # виртуальная настройка клавиатуры
 AUTO_SHIFT_ENABLE = no   # автоматический Шифт (при лёгком удержании) 
-MOUSEKEY_ENABLE = no    # эмуляция мышки
-#CONSOLE_ENABLE = no    # консоль
-COMMAND_ENABLE = no   # команды
-STENO_ENABLE = no    # стенография
+MOUSEKEY_ENABLE = no     # эмуляция мышки
+#CONSOLE_ENABLE = no     # консоль
+COMMAND_ENABLE = no      # команды
+STENO_ENABLE = no        # стенография
