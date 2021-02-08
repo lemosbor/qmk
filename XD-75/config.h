f6bb94a6e522aade037f9eb542f60fc6f0dcb75e
lemosbor <68635720+lemosbor@users.noreply.github.com>
2021-02-08 09:15:28 +0300

    Create config.h

---
+++ XD-75/config.h
@@ -0,0 +1,10 @@
+#pragma once
+#define LOCKING_SUPPORT_ENABLE // блокировка клавиш для капс локов
+#define LOCKING_RESYNC_ENABLE // синхронизация светодиода с каспами
+#define TAPPING_TERM 200 // задержка для клавиш двойного назначения
+#define COMBO_TERM 20 // задержка для комбо
+#define COMBO_COUNT 47 // количество спец. комбинаций с клавишами
+#define COMBO_ALLOW_ACTION_KEY
+
+#define ONESHOT_TAP_TOGGLE 2 // ожидающие кнопки. Количество нажатий для залипания.
+#define ONESHOT_TIMEOUT 2000  // ожидающие кнопки. время ожидания (мс)
