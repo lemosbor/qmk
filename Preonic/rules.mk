b6614c0e27a45f5ff4d59bf0048746f334183365
lemosbor <68635720+lemosbor@users.noreply.github.com>
2021-02-15 18:15:21 +0300

    Update rules.mk

--- Preonic/rules.mk
+++ Preonic/rules.mk
@@ -1,20 +1,21 @@
 #включить:
-UNICODEMAP_ENABLE = no   # поддержка карт юникода
 RGBLIGHT_ENABLE = yes     # поддержка светодиодов платы
 BACKLIGHT_ENABLE = yes    # поддержка светодиодов клавиш
 TAP_DANCE_ENABLE = yes    # поддержка двойных нажатий
-VELOCIKEY_ENABLE = no    # подстройка подсветки под скорость печати
 EXTRAKEY_ENABLE = yes     # аудиоконтроль и системный контроль
 COMBO_ENABLE = yes        # спец. комбинации из букв
-KEY_LOCK_ENABLE = yes
-ENCODER_ENABLE = yes
+ENCODER_ENABLE = yes      # поддержка энкодера (крутилки)
+
 
 #отключить:
 BLUETOOTH_ENABLE = no     # поддержка блютуза
 UNICODE_ENABLE = no       # поддержка юникода
 BOOTMAGIC_ENABLE = no     # виртуальная настройка клавиатуры
-AUTO_SHIFT_ENABLE = no   # автоматический Шифт (при лёгком удержании) 
-MOUSEKEY_ENABLE = yes
-#CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
-STENO_ENABLE = no
+AUTO_SHIFT_ENABLE = no    # автоматический Шифт (при лёгком удержании) 
+#CONSOLE_ENABLE = no      # консоль
+COMMAND_ENABLE = no       # команды
+STENO_ENABLE = no         # стенография
+UNICODEMAP_ENABLE = no    # поддержка карт юникода
+VELOCIKEY_ENABLE = no     # подстройка подсветки под скорость печати
+KEY_LOCK_ENABLE = no      # блокировка клавиш (удержание)
+MOUSEKEY_ENABLE = no      # эмуляция мышки
