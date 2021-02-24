9fea962afc2491e079107d8bf96f72602bfc0813
lemosbor <68635720+lemosbor@users.noreply.github.com>
2021-02-24 15:52:20 +0300

    Update keymap.c

--- XD-75/keymap.c
+++ XD-75/keymap.c
@@ -6,7 +6,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { // определ
   KC_C,          KC_X,         KC_U,        KC_GRV,   KC_LBRC,TD(POISK),KC_7,              KC_8,  KC_9, KC_F,      KC_H,        KC_P,     KC_L,     KC_M,  KC_V, \
   KC_I,          KC_A,         KC_E,        KC_O,     KC_S,   KC_ENT,   KC_4,              KC_5,  KC_6, KC_RBRC,   KC_K,        KC_N,     KC_T,     KC_W,  KC_R, \
   SFT_T(KC_BSLS),RU_E,         KC_Q,        KC_Y,     KC_NUBS,KC_DEL,   KC_1,              KC_2,  KC_3, KC_UP,     KC_G,        KC_D,     KC_B,     KC_Z,  KC_J, \
-  CTL_T(KC_ESC),LGUI_T(KC_TAB),ALT_T(KC_F2),KC_BSPC,KC_SPC,OSM(MOD_LSFT),LT(L_DOP, KC_PDOT),KC_0,KC_LEFT,KC_DOWN,SFT_T(KC_RGHT),TD(RU_AN),TD(NACH),TD(KONE),ALTTABB \
+  CTL_T(KC_ESC),LGUI_T(KC_TAB),ALT_T(KC_F2),KC_BSPC,KC_SPC,OSM(MOD_LSFT),LT(L_DOP, KC_PDOT),KC_0,KC_LEFT,KC_DOWN,SFT_T(KC_RGHT),TD(RU_AN),A_HOME,TD(KONE),ALTTABB \
 ),
 [L_DOP] = LAYOUT_ortho_5x15( \
   KC_TRNS,       KC_TRNS,      STEPE,       UMNO,     UDAR,    KC_TRNS, KC_F10,            KC_F11,KC_F12, KK_LBRC, KK_RBRC,     RU_TIR,   GRADU,    C(A(KC_DEL)),PS_1, \
