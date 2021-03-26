c1c23b7d8c99a84c2dbd694607b0d821397369ef
lemosbor <68635720+lemosbor@users.noreply.github.com>
2021-03-26 17:38:26 +0300

    Update keymap.c

--- Preonic/keymap.c
+++ Preonic/keymap.c
@@ -16,14 +16,14 @@ bool music_mask_user(uint16_t keycode) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { // определение матриц    
 [L_OSNOVA] = LAYOUT_preonic_grid( \
   TD(KOP1),     TD(VST1),TD(VYH), KC_COMM,    VOPR, KAVYCH, KC_SCLN,       SLESH,     TD(MINS),KC_DOT, TD(OSKOBT),  TD(ZSKOBT), \
-  KC_C,         KC_X,    KC_U,       KC_GRV,        KC_LBRC,TD(POISK),        KC_F,      KC_H,   KC_P,  KC_L,   KC_M,   KC_V, \
-  KC_I,         KC_A,    KC_E,       KC_O,          KC_S,   KC_TAB,KC_RBRC,   KC_K,   KC_N,  KC_T,   KC_W,   KC_R, \
-  KC_BSLS,      RU_E,    KC_Q,       KC_Y,          KC_NUBS,KC_DEL,        TD(RU_AN),    KC_G,   KC_D,  KC_B,   KC_Z,   KC_J, \
-  ALTTABB,TD(VYDEL),ALT_T(KC_F2),OSM(MOD_LSFT),CTL_T(KC_SPC),  KC_ENT,  LT(L_DOP, KC_BSPC),KC_SPC, KC_UP, KC_DOWN,KC_LEFT,KC_RGHT \
+KC_F,  KC_NUBS,  KC_I,  KC_Q,   KC_F15,   TD(POISK),  KC_RBRC,  KC_V,   KC_J,  KC_L,  KC_D,  KC_GRV,  \
+KC_S,  KC_E,  KC_A,  KC_O,  KC_Y,  KC_TAB,   KC_H,  KC_P,  KC_T,  KC_N,  KC_K,  KC_W,  \
+KC_LBRC,  KC_BSLS,  KC_X,  KC_U,  RU_E,  KC_DEL,    KC_C,  KC_Z,  KC_G,  KC_R,  KC_M,  KC_B,  \
+  ALTTABB,TD(RU_AN),ALT_T(KC_F2),OSM(MOD_LSFT),CTL_T(KC_SPC),  KC_ENT,  LT(L_DOP, KC_BSPC),KC_SPC, KC_UP, KC_DOWN,KC_LEFT,KC_RGHT \
 ),
 [L_DOP] = LAYOUT_preonic_grid( \
   RGB_TOG,      RGB_MOD,       S(KC_4), S(KC_5),      S(KC_7),     S(KC_2),   KC_F10,  KC_F11,  KC_F12,   KC_PAST,   KC_PPLS, KC_EQL,  \
-  KC_NUMLOCK,   RU_TY,          KC_TRNS,     KC_RESET,   KC_PSCR,  KC_TRNS, KC_F7,   KC_F8,   KC_F9,     KC_7,     KC_8,     KC_9, \
+  KC_NUMLOCK,   KC_TRNS,        KC_TRNS,     KC_RESET,   KC_PSCR,  KC_TRNS, KC_F7,   KC_F8,   KC_F9,     KC_7,     KC_8,     KC_9, \
   KC_MUTE,      KC_VOLD,        KC_VOLU,     KC_MSTP,   KC_MNXT,  KC_TRNS, KC_F4,   KC_F5,   KC_F6,     KC_4,     KC_5,     KC_6, \
   KC_TRNS,      KC_BRID,        KC_BRIU,     C(S(KC_ESC)),KC_TRNS,UD_STROK, KC_F1,   KC_F2,   KC_F3,     KC_1,     KC_2,     KC_3, \
   TO(L_MUSIC),     PS_1,           PS_2,        KC_TRNS, C(KC_PGUP),C(KC_PGDN),KC_TRNS, G_SP,  KC_PGUP,   KC_PGDN,  KC_0,     KC_PDOT \
