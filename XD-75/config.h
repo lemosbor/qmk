1a91e66dcc82a96b69e0b922f5fefe7c2876ef82
lemosbor <68635720+lemosbor@users.noreply.github.com>
2021-04-05 23:59:57 +0300

    Add files via upload

--- XD-75/config.h
+++ XD-75/config.h
@@ -5,9 +5,11 @@
 #define TAPPING_TERM 180 // задержка для клавиш двойного назначения
 #define PERMISSIVE_HOLD  // ускорение двойного нажатия
 // #define PERMISSIVE_HOLD_PER_KEY //  усорение двойного нажатия под каждую кнопку
-#define COMBO_COUNT 73 // кол-во комобо-команд
+#define COMBO_COUNT 79 // кол-во комобо-команд
 #define COMBO_TERM 20 // задержка для комбо
 #define COMBO_ALLOW_ACTION_KEY
 
 #define ONESHOT_TAP_TOGGLE 2 // ожидающие кнопки. Количество нажатий для залипания.
 #define ONESHOT_TIMEOUT 3000  // ожидающие кнопки. время ожидания (мс) 
+
+#define FORCE_NKRO
--- XD-75/rules.mk
+++ XD-75/rules.mk
@@ -1,8 +1,3 @@
-# Включить:
-BACKLIGHT_ENABLE = yes   # поддержка светодиодов клавиш
-TAP_DANCE_ENABLE = yes   # поддержка двойных нажатий
-COMBO_ENABLE = yes       # спец. комбинации из букв
-
 # Отключить:
 KEY_LOCK_ENABLE = no     # блокировка клавиш (удержание)
 RGBLIGHT_ENABLE = no     # поддержка светодиодов платы
@@ -18,3 +13,9 @@ COMMAND_ENABLE = no      # команды
 STENO_ENABLE = no        # стенография
 EXTRAKEY_ENABLE = no 	   # системные и аубиоконтольные клавиши
 SLEEP_LED_ENABLE = no    # спящее состояние
+# Включить:
+BACKLIGHT_ENABLE = yes   # поддержка светодиодов клавиш
+TAP_DANCE_ENABLE = yes   # поддержка двойных нажатий
+COMBO_ENABLE = yes       # спец. комбинации из букв
+STENO_ENABLE = yes       # стенография
+FORCE_NKRO = yes         # одновременное нажатие более 6 клавиш
